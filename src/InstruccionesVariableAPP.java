62c2f5b87b35bae9c20aba9e849a37e65debc4f7
Abel-Git2103 <abel_jornet@hotmail.com>
2022-01-12 02:26:42 +0100

    PRIMER COMMIT

---
+++ src/InstruccionesVariableAPP.java
@@ -0,0 +1,17 @@
+
+public class InstruccionesVariableAPP {
+
+	public static void main(String[] args) {
+		
+		int variableN = 25;
+		
+		System.out.println("Valor inicial de N = " + variableN);
+		for(; variableN <= 78; variableN+=77);
+			System.out.println("N + 77 = " + variableN);
+			
+		variableN-=3;
+		System.out.println("N - 3 = "+ variableN);
+		System.out.println("N * 2 = "+ (variableN * 2));
+					
+	}
+}
